904acc2316db219a75ac925af3239580692a8955
Дмитрий <@DESKTOP-FDRTOFA>
2017-01-26 16:43:15 +0400

    Добавьте файлы проекта.

---
+++ FBot/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBot
+{
+    public class Class1
+    {
+    }
+}
